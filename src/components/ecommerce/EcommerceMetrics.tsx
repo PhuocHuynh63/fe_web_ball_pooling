854f7e972bbe169bd20451a72c46f0c7fa4c1071
Anh Trí <Anhtri22303@gmail.com>
2025-02-16 23:13:59 +0700

    New logo & Delete Sidebar widget

--- src/components/ecommerce/EcommerceMetrics.tsx
+++ src/components/ecommerce/EcommerceMetrics.tsx
@@ -22,12 +22,12 @@ export default function EcommerceMetrics() {
               Customers
             </span>
             <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
-              3,782
+              8386
             </h4>
           </div>
           <Badge color="success">
             <ArrowUpIcon />
-            11.01%
+            0,001%
           </Badge>
         </div>
       </div>
@@ -41,16 +41,16 @@ export default function EcommerceMetrics() {
         <div className="flex items-end justify-between mt-5">
           <div>
             <span className="text-sm text-gray-500 dark:text-gray-400">
-              Orders
+              Tables
             </span>
             <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
-              5,359
+              4953
             </h4>
           </div>
 
           <Badge color="error">
             <ArrowDownIcon />
-            9.05%
+            99.99%
           </Badge>
         </div>
       </div>
--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -85,7 +85,7 @@ const AppHeader: React.FC = () => {
           <Link to="/" className="lg:hidden">
             <img
               className="dark:hidden"
-              src="./images/logo/logo.svg"
+              src="./images/logo/logo.svg" 
               alt="Logo"
             />
             <img
@@ -144,7 +144,6 @@ const AppHeader: React.FC = () => {
 
                 <button className="absolute right-2.5 top-1/2 inline-flex -translate-y-1/2 items-center gap-0.5 rounded-lg border border-gray-200 bg-gray-50 px-[7px] py-[4.5px] text-xs -tracking-[0.2px] text-gray-500 dark:border-gray-800 dark:bg-white/[0.03] dark:text-gray-400">
                   <span> ⌘ </span>
-                  <span> K </span>
                 </button>
               </div>
             </form>
